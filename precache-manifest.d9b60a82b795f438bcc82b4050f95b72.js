d914debaabc9a7e53737ecea3e6885aecac9d65f
Subhash Kumar <subhkum2@cisco.com>
2020-04-16 01:48:53 +0530

    Updates

---
+++ precache-manifest.d9b60a82b795f438bcc82b4050f95b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a8a65f9c58d3799f544bfaf6fb8d9c",
+    "url": "/covid19dailyTracker/index.html"
+  },
+  {
+    "revision": "25d2c3572de593619996",
+    "url": "/covid19dailyTracker/static/css/main.bedaa60a.chunk.css"
+  },
+  {
+    "revision": "679f9c79f733314bd4b1",
+    "url": "/covid19dailyTracker/static/js/2.fad296d5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19dailyTracker/static/js/2.fad296d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d2c3572de593619996",
+    "url": "/covid19dailyTracker/static/js/main.e9711c44.chunk.js"
+  },
+  {
+    "revision": "d2f9b1f8f137a6d16931",
+    "url": "/covid19dailyTracker/static/js/runtime-main.11918c1b.js"
+  }
+]);
